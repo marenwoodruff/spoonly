f7c309404ecd124b098afcb073e4047f2238f1c7
MarenWoodruff <wren403@gmail.com>
2014-02-13 13:57:03 -0800

    changed db:seed file

--- config/application.rb
+++ config/application.rb
@@ -25,17 +25,17 @@ module Spoonly
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-  #   config.paperclip_defaults = {
-  #     storage: :fog,
-  #     fog_credentials: {
-  #       provider: "Google",
-  #       google_storage_access_key_id: ENV['GSTORAGE_ACCESS_KEY_ID'],
-  #       google_storage_secret_access_key: ENV['GSTORAGE_SECRET_ACCESS_KEY']
-  #     },
-  #     fog_public: true,
-  #     fog_host: ENV['GSTORAGE_HOST'],
-  #     fog_directory: ENV['GSTORAGE_BUCKET'],
-  #     path: ':class/:attachment/:id/:style/:filename'
-  #   }
+    config.paperclip_defaults = {
+      storage: :fog,
+      fog_credentials: {
+        provider: "Google",
+        google_storage_access_key_id: ENV['GSTORAGE_ACCESS_KEY_ID'],
+        google_storage_secret_access_key: ENV['GSTORAGE_SECRET_ACCESS_KEY']
+      },
+      fog_public: true,
+      fog_host: ENV['GSTORAGE_HOST'],
+      fog_directory: ENV['GSTORAGE_BUCKET'],
+      path: ':class/:attachment/:id/:style/:filename'
+    }
   end
 end
